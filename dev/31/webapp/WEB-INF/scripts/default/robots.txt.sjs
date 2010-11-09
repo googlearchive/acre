38479fd9837d8ce6b05a88a486250753d4a7c0ea
masouras@google.com <masouras@google.com@c73c2f23-f350-738c-ca6e-3bcbe9730263>
2010-11-09 00:21:40 +0000

    cut branch for release 31

---
+++ dev/31/webapp/WEB-INF/scripts/default/robots.txt.sjs
@@ -0,0 +1,5 @@
+acre.response.status = 200;
+acre.response.content_type = 'text/plain';
+
+acre.write("User-agent: * \n");
+acre.write("Disallow: /acre/ \n");
---
+++ dev/31/webapp/WEB-INF/src/com/google/util/javascript/DOM/JSDOMParserException.java
@@ -0,0 +1,59 @@
+package com.google.util.javascript.DOM;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Function;
+import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.ScriptableObject;
+
+import com.google.util.javascript.JSObject;
+
+public class JSDOMParserException extends JSObject {
+
+    private static final long serialVersionUID = 1458963046407607404L;
+
+    public String message;
+    public int lineNumber;
+    public int columnNumber;
+
+    public JSDOMParserException() { }
+
+    public JSDOMParserException(String msg, int ln, int coln, Scriptable scope) {
+        message = msg;
+        lineNumber = ln;
+        columnNumber = coln;
+        _scope = scope;
+     }
+
+    public static Scriptable jsConstructor(Context cx, Object[] args, Function ctorObj, boolean inNewExpr) {
+        Scriptable scope = ScriptableObject.getTopLevelScope(ctorObj);
+        return new JSDOMParserException(((JSDOMParserException)args[0]).message, ((JSDOMParserException)args[0]).lineNumber, ((JSDOMParserException)args[0]).columnNumber, scope);
+    }
+
+    public String getClassName() {
+        return "DOMParserException";
+    }
+
+    public String jsGet_message() {
+        return message;
+    }
+
+    public void jsSet_message(String msg) {
+        message = msg;
+    }
+
+    public int jsGet_lineNumber() {
+        return lineNumber;
+    }
+
+    public int jsGet_columnNumber() {
+        return columnNumber;
+    }
+
+    public String toString() {
+        return "[DOMParserException error: " + message + " at line " + lineNumber + " and column " + columnNumber +"]";
+    }
+
+    public String jsFunction_toString() {
+        return "[DOMParserException error: " + message + " at line " + lineNumber + " and column " + columnNumber +"]";
+    }
+}
