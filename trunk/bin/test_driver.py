a4262ae499ee1d3499fc0a59a653a6ce48074553
bneutra@google.com <bneutra@google.com@c73c2f23-f350-738c-ca6e-3bcbe9730263>
2011-01-19 20:20:30 +0000

    output test failures

--- trunk/bin/test_driver.py
+++ trunk/bin/test_driver.py
@@ -40,7 +40,7 @@ class colors:
 #-----------------------------------------------------------------------# 
                         
 def drive_test(url):
-    test = urllib2.urlopen(url + "?output=jsonsummary")
+    test = urllib2.urlopen(url + "?output=json")
     return simplejson.loads(test.read())
                 
 def drive_app(app,color):
@@ -57,10 +57,12 @@ def drive_app(app,color):
     
     total_failures = 0
     total_tests = 0
+    fail_log = ""
     
     for t in results['testfiles']:
         test_url = t['run_url']
-        test_results = drive_test(test_url)
+        data = drive_test(test_url)
+        test_results = data['testfiles'][0]
         tests = test_results['total'] or 0
         failures = test_results['failures'] or 0
         total_tests += tests
@@ -76,6 +78,9 @@ def drive_app(app,color):
         if color:
             out.write(colors.RESET)
         out.write("\n")
+        if failures > 0:
+            fail_log += get_fail_log(test_results)
+            fail_log += "-"*80 + "\n"
         
     ret = 0
     out.write("\n")
@@ -93,8 +98,34 @@ def drive_app(app,color):
     if total_tests == 0:
         out.write("no tests were found or run\n")
         ret = 1
+    if fail_log: 
+        print "*** failures ***"
+        print "-"*80
+        print fail_log
     return ret
-        
+
+def get_fail_log(obj):
+    out = obj['run_url'] + "\n"
+    for o in obj['modules']:
+        testout = ""
+        mname = o['name']
+        if mname == "DEFAULT":
+            mname = ""
+        else:
+            mname = " [%s]" % mname
+        for t in o['tests']:
+            name = t['name']
+            if t.get('log') is None: continue
+            for l in t['log']:
+                res = l.get('result')
+                msg = l.get('message')
+                if not msg: msg = "fail but no message found"
+                if res is False or res is None:
+                    testout += "  %s\n" % msg
+        if testout:
+            out +=  "%s%s:\n%s" % (name, mname, testout)
+    return out
+
 #-----------------------------------------------------------------------# 
 
 def usage(msg=''):
