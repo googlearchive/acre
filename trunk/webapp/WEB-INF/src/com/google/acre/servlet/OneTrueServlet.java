7c815329f6c118ee4c445b32d13cf874b3aae270
stefanom@google.com <stefanom@google.com@c73c2f23-f350-738c-ca6e-3bcbe9730263>
2011-02-19 10:08:50 +0000

    whitespace (no functional changes)

--- trunk/webapp/WEB-INF/src/com/google/acre/servlet/OneTrueServlet.java
+++ trunk/webapp/WEB-INF/src/com/google/acre/servlet/OneTrueServlet.java
@@ -105,19 +105,17 @@ public class OneTrueServlet extends javax.servlet.http.HttpServlet implements ja
         if (rule_host == null) return true;
 
         if (rule_host.startsWith(".")) {
-            
-            if (request_host.endsWith(rule_host))
+            if (request_host.endsWith(rule_host)) {
                 return true;
-
+            }
             String base_host = rule_host.substring(1);
-            if (request_host.equals(base_host))
+            if (request_host.equals(base_host)) {
                 return true;
-                
+            }
         } else {
-            
-            if (rule_host.equals(request_host))
+            if (rule_host.equals(request_host)) {
                 return true;
-                
+            }
         }
 
         return false;
