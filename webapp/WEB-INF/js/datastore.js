4b014c1492770d257749708c65b248baf8d03cb3
stefanom <stefanom@google.com>
2011-03-22 08:07:09 +0000

    if the appid is a guid, compress it so that the acre keystore keys are not so big (as this guid is added to every part of the key and gets huge really fast when base64 encoded)

--- webapp/WEB-INF/js/datastore.js
+++ webapp/WEB-INF/js/datastore.js
@@ -20,9 +20,51 @@ var augment;
 
     };
 
+    var graphd_guid_prefix = "#9202a8c04000641f8";
+
+    var keyStr = "_ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-=";
+
+
+    // turn a number into its base64 representation
+    function compress_num(n) {
+      var o = "";
+      
+      for (var i = 0; i < 5; i++) {
+        var mask = (1 << (6*(i+1))) - 1;
+        var masked = n & mask;
+        var adjusted = masked >> (6*i);
+        o += keyStr[adjusted];
+      }
+      
+      return o;
+    }
+
+    // remove trailing underscores since they don't provide any differentiation signal
+    function remove_trailing_underscores(str) {
+        for (var i = str.length - 1; str.charAt(i) == "_"; i--);
+        return str.substring(0,i+1);
+      }
+    
+    // given a freebase guid, return the shortest possible string
+    // that we can use to keep unicity between apps
+    function compress(guid) {
+      if (guid.indexOf(graphd_guid_prefix) != 0) return guid;
+      var str = guid.substring(graphd_guid_prefix.length);
+      var str1 = str.substring(0,7);
+      var str2 = str.substring(8,15);
+      var sep_ch = str.charAt(7);
+      var sep = parseInt(sep_ch,16);
+      var n1 = (parseInt(str1,16) << 2) + (sep >> 2);
+      var n2 = parseInt(str2,16) + (sep << 28);
+      var c1 = compress_num(n1);
+      var c2 = compress_num(n2);
+      return remove_trailing_underscores(c2 + c1);
+    }
+    
     function get_appid() {
         var appid = acreboot._request_app_guid;
         if (!appid) throw Error("appid can't be null or undefined");
+        appid = compress(appid);
         return appid;
     }
 
